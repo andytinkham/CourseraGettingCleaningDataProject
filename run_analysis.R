76af88b39dcf9e0c0812ca1f52699e751dcd192b
andytinkham <andy@tinkham.org>
2018-04-08 18:39:22 -0500

    Read in the files and merged the dataset. Added data files to the .gitignore

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,60 @@
 # Getting and Cleaning Data course, final project
 # Andy Tinkham, April 8, 2018
 
+library(data.table)
+
 # Retrieve the data if it doesn't already exist in the working directory
+if (!file.exists("./UCIHARDataSet.zip")) {
+        fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        download.file(fileUrl, destfile = "./UCIHARDataSet.zip",
+                      method = "curl")
+
+        # Unzip the data. Overwrite any files that might already exist with the
+        # same names to make sure the data we downloaded remains consistent
+        unzip("./UCIHARDataSet.zip", overwrite = TRUE)
+}
+
+# Read in the following files:
+# ./UCI HAR Dataset/activity_labels.txt
+# ./UCI HAR Dataset/features.txt
+# ./UCI HAR Dataset/test/subject_test.txt
+# ./UCI HAR Dataset/test/X_test.txt
+# ./UCI HAR Dataset/test/y_test.txt
+# ./UCI HAR Dataset/train/subject_train.txt
+# ./UCI HAR Dataset/train/X_train.txt
+# ./UCI HAR Dataset/train/y_train.txt
+
+setwd("./UCI HAR Dataset")
+
+# Overall descriptors
+activity_labels <- read.table("./activity_labels.txt", sep = " ",
+                              col.names = c("activityid", "activitydescription"))
+
+features <- read.table("./features.txt",
+                       col.names = c("featureid", "measurementname"))
+
+# Test data set
+subject_test <-  read.table("./test/subject_test.txt", col.names = "subjectid")
+
+X_test <- read.table("./test/X_test.txt")
+
+y_test <- read.table("./test/y_test.txt", col.names = "activityid")
+
+testdataset <- data.table(subject_test, X_test, y_test)
+
+# Training data set
+subject_train <- read.table("./train/subject_train.txt", col.names = "subjectid")
+
+X_train <- read.table("./train/X_train.txt")
+
+y_train <- read.table("./train/y_train.txt", col.names = "activityid")
+
+traindataset <- data.table(subject_train, X_train, y_train)
+
+setwd("..")
 
 # Merge the training and test sets into one data set
+dataset <- rbind(traindataset, testdataset)
 
 # Extract only the measurements on the mean and standard deviation for each
 # measurement
