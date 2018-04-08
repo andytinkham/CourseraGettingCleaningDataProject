07d9a96f8ba8ff502410db9c020bf763f71e4df7
andytinkham <andy@tinkham.org>
2018-04-08 14:01:37 -0500

    Added skeleton comments for needed tasks

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+# Johns Hopkins University Coursera Data Science Specialization
+# Getting and Cleaning Data course, final project
+# Andy Tinkham, April 8, 2018
+
+# Retrieve the data if it doesn't already exist in the working directory
+
+# Merge the training and test sets into one data set
+
+# Extract only the measurements on the mean and standard deviation for each
+# measurement
+
+# Use descriptive activity names to name the activities in the data set
+
+# Appropriately label the data set with descriptive variable names
+
+# Create a 2nd independent tidy data set with the average of each variable for
+# each activity and each subject
+
+# Output the data set
+
+# Write tidy data set to text file with write.table using row.name = FALSE
